e6463c44e37229bda12c1d8d9f978c42ddff4ecb
PauloRibeiro167 <146147732+PauloRibeiro167@users.noreply.github.com>
2024-10-18 19:55:54 +0000

    Add server startup logs to server.log

--- start_server.sh
+++ start_server.sh
@@ -32,29 +32,42 @@ show_progress_vite() {
   echo -ne "\n"
 }
 
+# Função para matar processos pelo PID
+kill_process() {
+  local pid=$1
+  if [ -n "$pid" ]; then
+    kill -9 $pid
+    echo "Process $pid has been killed."
+  fi
+}
+
 # Verificar se o servidor Rails já está rodando
 rails_pid=$(pgrep -f "rails server")
 if [ -n "$rails_pid" ]; then
-  echo "Rails server is already running (PID: $rails_pid)."
-else
-  echo "Starting Rails server..."
-  show_progress_rails 10 &
-  bin/rails server -d -p 3000
-  wait $!
-  rails_pid=$(pgrep -f "rails server")
+  echo "Rails server is already running (PID: $rails_pid). Killing process..." >> server.log
+  kill_process $rails_pid
 fi
 
 # Verificar se o servidor Vite já está rodando
 vite_pid=$(pgrep -f "vite")
 if [ -n "$vite_pid" ]; then
-  echo "Vite server is already running (PID: $vite_pid)."
-else
-  echo "Starting Vite server..."
-  (cd frontend/web && npm run dev > vite.log 2>&1) &
-  show_progress_vite 10 &
-  wait $!
-  vite_pid=$(pgrep -f "vite")
+  echo "Vite server is already running (PID: $vite_pid). Killing process..." >> server.log
+  kill_process $vite_pid
 fi
+
+echo -e "\e[31mStarting Rails server...\e[0m" >> server.log
+show_progress_rails 10 &
+rails_progress_pid=$!
+bin/rails server -d -p 3000 > rails.log 1>&1
+wait $rails_progress_pid
+rails_pid=$(pgrep -f "rails server")
+
+echo -e "\e[36mStarting Vite server...\e[0m" >> server.log
+show_progress_vite 10 &
+vite_progress_pid=$!
+(cd frontend/web && npm run dev > vite.log 2>&1) &
+wait $vite_progress_pid
+vite_pid=$(pgrep -f "vite")
 
 # Esperar os servidores iniciarem
 wait
@@ -65,12 +78,12 @@ vite_pid=$(pgrep -f "vite")
 
 # Verificar se os PIDs foram capturados corretamente
 if [ -z "$rails_pid" ]; then
-  echo "Error: Rails server failed to start."
+  echo "Error: Rails server failed to start." >> server.log
   exit 1
 fi
 
 if [ -z "$vite_pid" ]; then
-  echo "Error: Vite server failed to start."
+  echo "Error: Vite server failed to start." >> server.log
   exit 1
 fi
 
@@ -84,8 +97,9 @@ server_info=(
 )
 
 # Exibir mensagem de sucesso
-echo -e "\e[37mStart to success!\e[0m"
-echo -e "\e[34mRails server PID: ${server_info[rails_pid]}\e[0m"
-echo -e "\e[34mRails server running on ${server_info[rails_url]}\e[0m"
-echo -e "\e[34mVite server PID: ${server_info[vite_pid]}\e[0m"
-echo -e "\e[34mVite server running on ${server_info[vite_url]}\e[0m"
+echo -e "\e[37mStart to success!\e[0m" >> server.log
+echo -e "\e[34mRails server PID: ${server_info[rails_pid]}\e[0m" >> server.log
+echo -e "\e[34mRails server running on ${server_info[rails_url]}\e[0m" >> server.log
+echo -e "\e[34mVite server PID: ${server_info[vite_pid]}\e[0m" >> server.log
+echo -e "\e[34mVite server running on ${server_info[vite_url]}\e[0m" >> server.log
+
