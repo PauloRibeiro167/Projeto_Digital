9dc40130e13caebf2a71099415b11eb386f06654
PauloRibeiro167 <146147732+PauloRibeiro167@users.noreply.github.com>
2024-10-22 17:21:04 +0000

    Add custom logger middleware to vite.config.js

--- frontend/web/vite.config.js
+++ frontend/web/vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'custom-logger',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          console.log(`${req.method} ${req.url}`);
+          next();
+        });
+      }
+    }
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
