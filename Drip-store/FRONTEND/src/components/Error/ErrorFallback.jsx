fa01ae1ea1aa649208e023da5bb36860cf7e4353
PauloRibeiro167 <146147732+PauloRibeiro167@users.noreply.github.com>
2024-10-30 19:47:52 +0000

    "Feat: Correção de ErrorFallback.jsx para congruência com o App.jsx "

--- Drip-store/FRONTEND/src/components/Error/ErrorFallback.jsx
+++ Drip-store/FRONTEND/src/components/Error/ErrorFallback.jsx
@@ -1,11 +1,10 @@
-// src/components/ErrorBoundary/ErrorFallback.jsx
 import React from 'react';
 
 function ErrorFallback({ resetErrorBoundary }) {
   return (
     <div role="alert">
       <p>Erro 500: Algo deu errado.</p>
-      <button onClick={re}>Tentar novamente</button>
+      <button onClick={resetErrorBoundary}>Tentar novamente</button>
     </div>
   );
 }
